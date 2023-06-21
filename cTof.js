0b0f20e263a6a3275cac2b57fe5bd4bf84b36bc3
Glory-Lyka Okposin <lykaedem11@gmail.com>
2023-06-21 03:30:50 +0100

    JS Algorithms

---
+++ cTof.js
@@ -0,0 +1,10 @@
+// Temperature convertion.
+function convertCtoF(celsius) {
+  // stating the formula for convertion of Celsius to Fahrenheit.
+  const fahrenheit = celsius * (9/5)+ 32;
+  console.log(fahrenheit);
+}
+
+// Test mode
+convertCtoF(100);
+convertCtoF(27.4);
